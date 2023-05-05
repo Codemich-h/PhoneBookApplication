bedce27eb555a694245a8b9caf2d7755e59ad2d2
Codemich-h <michebastos61@gmail.com>
2023-05-05 19:37:43 +0530

    React Nav-Bar using Router

---
+++ phonebook/src/components/EditPhoneDetails.js
@@ -0,0 +1,16 @@
+function EditdPhoneDetails() {
+    return(
+        <div>
+            
+            <form>
+                <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+                <h1 className="text-3xl font-semibold text-center text-black-700 ">Login</h1>
+                 </div>
+
+                 
+            </form>
+        </div>    
+    )
+}
+
+export default EditdPhoneDetails
---
+++ phonebook/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+function Login() {
+
+    return(
+            <form className="mt-6">
+                <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+            <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+                <h1 className="text-3xl font-semibold text-center text-black-700 ">Login</h1>
+            </div>    
+            </div>
+            </form>
+    )
+}
+export default Login;
