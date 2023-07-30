49539e8034bb7a1232ddbb8dd82729c538bfb325
Codemich-h <michebastos61@gmail.com>
2023-07-30 21:56:28 +0530

    Edit button completed

--- phonebook/src/pages/Contacts.js
+++ phonebook/src/pages/Contacts.js
@@ -85,8 +85,11 @@ function Contacts() {
                 <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
                 <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>   
             </div>
-        <input type='text' className="block w-small p-4 pl-10 text-sm text-black-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."></input>
-    </div> 
+            <div class="pt-2 relative mx-auto text-gray-600">
+                <input class="border-2 border-gray-300 bg-white  h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none" type="search" name="search" placeholder="Search" />
+            
+        </div>
+        </div> 
     <div className="mt-2 mr-16 flex flex-col">
         <div className="-my-2 overflow-x-auto -mx-4 sm:-mx-6 lg:-mx-8">
             <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
--- phonebook/src/pages/UpdateContact.js
+++ phonebook/src/pages/UpdateContact.js
@@ -2,33 +2,56 @@ import React from 'react';
 import { httpCall } from '../api/httpCall';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
 
 function UpdateContact () {
     var {id} = useParams();
-    const userData = {
+    const navigate = useNavigate()
+
+    const [data, setData] = useState({
         name: "",
         phone_number: "",
         relationship: "",
         country: "",
         email: "",
         job : ""
+    });
+
+    const getData = {
+        name: data.name,
+        phone_number: data.phone_number,
+        relationship: data.relationship,
+        country: data.country,
+        email: data.email,
+        job : data.job
     }
-    const [data, setData] = useState(userData);
     const handleData = (e) => {
         setData({...data, [e.target.name]: e.target.value})
     }
-   
-    useEffect(() => {
-        httpCall().put('update-contact/' + id) 
+    const submitData = (e) => {
+        e.preventDefault();
+        httpCall().put(`update-contact/${id}`, getData) 
         .then(res => {
-            return res.json();
-        })
-        .then(res => {
-            setData(res.data);
+            JSON.stringify(data);
+            setData(res.data)
+            alert(res.data.message)
+            console.log(res.data)
+            navigate('/', {return: true})
         })
         .catch(error => {
             console.log("Error:" + error)
+        })   
+    }
+   
+    useEffect(() => {  
+        // eslint-disable-next-line no-restricted-globals, no-undef
+        httpCall().get(`http://127.0.0.1:8000/api/v1/get-contact/${id}`, {getData})
+        .then(res => {
+            setData(res.data.data)
+            console.log(res.data)
+        })
+        .catch((error) => {
+            console.log('Error:' + error)
         })
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
@@ -37,16 +60,16 @@ function UpdateContact () {
   <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
       <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Update Contact
+              Edit Contact
           </h1>
-          <form   className="space-y-4 md:space-y-6" action="#">
+          <form onSubmit={submitData}  className="space-y-4 md:space-y-6" action="#">
               <div>
                   <label for="name" className=" block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
                   <input type="text" value={data.name} onChange={handleData}  name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your name" required=""></input>
               </div>
               <div>
                   <label for="phone_number" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
-                  <input type="phone_number" value={data.phone_number} onChange={handleData} name="phone_number" id="phone_number" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="phone_number" required=""></input>
+                  <input type="phone_number" value={data.phone_number}  onChange={handleData} name="phone_number" id="phone_number" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="phone_number" required=""></input>
               </div>
               <div>
                   <label for="relationship" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Relationship</label>
@@ -62,7 +85,7 @@ function UpdateContact () {
               </div>
                 <div>
                   <label for="job" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job</label>
-                  <input  type="text" value={data.job} onChange={handleData} name="job" id="job" placeholder="Dev" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""></input>
+                  <input  type="text" value={data.job} onChange={handleData}  name="job" id="job" placeholder="Dev" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""></input>
                 </div>
               <button type="submit" className="w-full text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Update Contact</button>
           </form>
